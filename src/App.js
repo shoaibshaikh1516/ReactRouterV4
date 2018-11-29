7454c24074577b5f98099504fb33b864e6ab56f0
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-11-29 15:36:32 +0530

    Now lastly, when we’re at the /topics route, if a topic hasn’t already been selected, we want to render some text that says “Please select a topic”. We can make a component that renders that text or we can just use Routes render prop like so

--- src/App.js
+++ src/App.js
@@ -23,13 +23,12 @@ const About = () => (
   </div>
 )
 
-
-
 const Topic = ({ match }) => (
   <div>
     <h3>{match.params.topicId}</h3>
   </div>
 )
+
 const Topics = ({ match }) => (
   <div>
     <h2>Topics</h2>
@@ -52,6 +51,9 @@ const Topics = ({ match }) => (
     </ul>
 
      <Route path={`${match.url}/:topicId`} component={Topic} />
+     <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
   </div>
 )
 
